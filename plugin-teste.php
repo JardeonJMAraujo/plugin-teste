918b64fbbcdc93514afbbd453ff3bb5c629dff5d
Jardeon J M Araujo <70022376+JardeonJMAraujo@users.noreply.github.com>
2023-08-17 21:23:41 -0300

    commit inicial

---
+++ plugin-teste.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Teste-plugin
+ * Plugin URI:        https://github.com/JardeonJMAraujo/plugin-teste.git
+ * Description:       Plugin de teste WP
+ * Version:           1.0.0
+ * Author:            JJDev - Jardeon J M Araujo
+ *
+ */
